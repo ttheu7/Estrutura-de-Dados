2ba3552275725c7e4b8c1f76b5e99b6e32334a13
Matheus Gonçalves <102565900+ttheu7@users.noreply.github.com>
2025-09-24 22:07:11 -0300

    Binary Tree Commit

---
+++ Trees/BinaryTree/BinaryTree/BinaryTree.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    public class BinaryTree<T>
+    {
+        public BinaryTreeNode<T>? Root { get; set; }
+        public int Count { get; set; }
+        public enum TraversalEnum
+        {
+            PREORDER, INORDER, POSTORDER
+        }
+
+        private void TraversePreOrder(BinaryTreeNode<T> node,
+        List<BinaryTreeNode<T>> result)
+        {
+            if (node != null)
+            {
+                result.Add(node);
+                TraversePreOrder(node.Left, result);
+                TraversePreOrder(node.Right, result);
+            }
+        }
+
+        private void TraverseInOrder(BinaryTreeNode<T> node,
+        List<BinaryTreeNode<T>> result)
+        {
+            if (node != null)
+            {
+                TraverseInOrder(node.Left, result);
+                result.Add(node);
+                TraverseInOrder(node.Right, result);
+            }
+        }
+
+        private void TraversePostOrder(BinaryTreeNode<T> node,
+        List<BinaryTreeNode<T>> result)
+        {
+            if (node != null)
+            {
+                TraversePostOrder(node.Left, result);
+                TraversePostOrder(node.Right, result);
+                result.Add(node);
+            }
+        }
+
+        public List<BinaryTreeNode<T>> Traverse(TraversalEnum mode)
+        {
+            List<BinaryTreeNode<T>> nodes = new List<BinaryTreeNode<T>>();
+            switch (mode)
+            {
+                case TraversalEnum.PREORDER:
+                    TraversePreOrder(Root!, nodes);
+                    break;
+                case TraversalEnum.INORDER:
+                    TraverseInOrder(Root!, nodes);
+                    break;
+                case TraversalEnum.POSTORDER:
+                    TraversePostOrder(Root!, nodes);
+                    break;
+            }
+            
+            return nodes;
+        }
+    }
+}
---
+++ Trees/BinaryTree/BinaryTree/BinaryTreeNode.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    public class BinaryTreeNode<T> : TreeNode<T>
+    {
+        public BinaryTreeNode() => Children = new List<TreeNode<T>>() { null!, null! };
+
+        public BinaryTreeNode<T> Left
+        {
+            get { return (BinaryTreeNode<T>)Children[0]; }
+            set { Children[0] = value; }
+        }
+
+        public BinaryTreeNode<T> Right
+        {
+            get { return (BinaryTreeNode<T>)Children[1]; }
+            set { Children[1] = value; }
+        }
+    } 
+}
