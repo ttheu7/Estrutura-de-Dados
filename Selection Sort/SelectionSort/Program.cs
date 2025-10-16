d8e943ac554cd482eca9c57d292e86ddaf8662ba
Matheus Gonçalves <102565900+ttheu7@users.noreply.github.com>
2025-10-15 21:52:24 -0300

    Selection Sort Commit

---
+++ Selection Sort/SelectionSort/Program.cs
@@ -0,0 +1,11 @@
+﻿using SelectionSort;
+
+int[] integerValues = { -11, 12, -42, 0, 1, 90, 68, 6, -9 };
+SelectionSort.SelectionSort.Sort(integerValues);
+Console.WriteLine(string.Join(" | ", integerValues));
+
+Console.WriteLine("---------------------------------------");
+
+string[] stringValues = { "Caneta", "Azul", "Thais", "Confessing", "Jacaré Bangão" };
+SelectionSort.SelectionSort.Sort(stringValues);
+Console.WriteLine(string.Join(" | ", stringValues));
---
+++ Selection Sort/SelectionSort/SelectionSort.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SelectionSort
+{
+    public static class SelectionSort
+    {
+        public static void Sort<T>(T[] array)
+            where T : IComparable
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int minIndex = i;
+                T minValue = array[i];
+
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j].CompareTo(minValue) < 0)
+                    {
+                        minIndex = j;
+                        minValue = array[j];
+                    }
+                }
+
+                Swap(array, i, minIndex);
+            }
+        }
+
+        public static void Swap<T>(T[] array, int first, int second)
+        {
+            T temp = array[first];
+            array[first] = array[second];
+            array[second] = temp;
+        }
+    }
+    
+}
