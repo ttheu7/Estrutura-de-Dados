bdcba70fbff6857f300d68a5056df8d87bc51634
Matheus Gonçalves <102565900+ttheu7@users.noreply.github.com>
2025-10-22 21:07:16 -0300

    QuickSort Commit

---
+++ QuickSort/QuickSort/Program.cs
@@ -0,0 +1,12 @@
+﻿using QuickSort;
+
+int[] integerValues = { -11, 12, -42, 0, 1, 90, 68, 6, -9 };
+QuickSort.QuickSort.Sort(integerValues);
+
+Console.WriteLine(string.Join(", ", integerValues));
+
+Console.WriteLine("---------------------------------------");
+
+string[] stringValues = { "Caneta", "Azul", "Thais", "Confessing", "Jacaré Bangão" };
+QuickSort.QuickSort.Sort(stringValues);
+Console.WriteLine(string.Join(" | ", stringValues));
