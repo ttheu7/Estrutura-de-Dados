39eab049f5f81b6eb4d92f19ea447d423ee24a3e
Matheus Gonçalves <102565900+ttheu7@users.noreply.github.com>
2025-10-29 20:01:35 -0300

    BogoSort

---
+++ BogoSort/BogoSort/BogoSort.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Boguinho
+{
+    public static class BogoSort
+    {
+        private static Random random = new Random();
+    
+        public static void Sort(int[] array)
+        {
+            int tries = 0;
+            
+            while (!IsOrdenad(array))
+            {
+                Shuffle(array);
+                tries++;
+            }
+            
+            Console.WriteLine($"\nNúmero de tentativas: {tries}");
+        }
+        
+        private static bool IsOrdenad(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] > array[i + 1])
+                    return false;
+            }
+            return true;
+        }
+        
+        private static void Shuffle(int[] array)
+        {
+            for (int i = array.Length - 1; i > 0; i--)
+            {
+                int j = random.Next(i + 1);
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+}
---
+++ BogoSort/BogoSort/Program.cs
@@ -0,0 +1,11 @@
+﻿using Boguinho;
+
+int[] array = { 5, 2, 8, 1, 9, 15, 58, 77, -1 };
+
+Console.WriteLine("Array original:");
+Console.WriteLine(string.Join(", ", array));
+
+BogoSort.Sort(array);
+
+Console.WriteLine("\nArray ordenado:");
+Console.WriteLine(string.Join(", ", array));
