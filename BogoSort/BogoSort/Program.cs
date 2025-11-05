7c2aba2af09fecd14c6a050d546cb9e7c0b456a8
Matheus Gonçalves <102565900+ttheu7@users.noreply.github.com>
2025-11-05 20:29:08 -0300

    BogoSort

---
+++ BogoSort/BogoSort/Program.cs
@@ -0,0 +1,22 @@
+﻿using Boguinho;
+
+int[] array = { 5, 2, 8, 1, 9, 15, 58, 77, -1 };
+string[] arrayA = {"Chico", "Sabo", "Tesao", "Aba", "Doodles" };
+
+Console.WriteLine("Array original (Números):");
+Console.WriteLine(string.Join(", ", array));
+
+BogoSort.Sort(array);
+
+Console.WriteLine("\nArray ordenado:");
+Console.WriteLine(string.Join(", ", array));
+
+Console.WriteLine("\n========================================\n");
+
+Console.WriteLine("Array original (Strings):");
+Console.WriteLine(string.Join(", ", arrayA));
+
+BogoSort.Sort(arrayA);
+
+Console.WriteLine("\nArray ordenado:");
+Console.WriteLine(string.Join(", ", arrayA));
