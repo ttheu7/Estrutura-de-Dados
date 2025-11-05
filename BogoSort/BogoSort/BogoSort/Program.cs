974c5313274617b202061706a37c32dc7395d488
Matheus Gonçalves <102565900+ttheu7@users.noreply.github.com>
2025-11-05 19:59:45 -0300

    Bogo com Tesão

---
+++ BogoSort/BogoSort/BogoSort/Program.cs
@@ -0,0 +1,12 @@
+﻿using Boguinho;
+
+int[] array = { 5, 2, 8, 1, 9, 15, 58, 77, -1 };
+string[] arrayA = {"Chico", "Sabo", "Tesao", "Aba", "Doodles" };
+
+Console.WriteLine("Array original:");
+Console.WriteLine(string.Join(", ", arrayA));
+
+BogoSort.Sort(arrayA);
+
+Console.WriteLine("\nArray ordenado:");
+Console.WriteLine(string.Join(", ", arrayA));
