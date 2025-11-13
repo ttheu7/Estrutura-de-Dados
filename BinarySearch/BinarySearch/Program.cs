ef17d1d8dd2433f125a0d1845455ee0d5d00eae4
Matheus Gonçalves <102565900+ttheu7@users.noreply.github.com>
2025-11-12 22:08:16 -0300

    SearchStructures

---
+++ BinarySearch/BinarySearch/Program.cs
@@ -0,0 +1,60 @@
+﻿static object BinarySearchIterative<T>(T[] inputArray, T key) where T : IComparable<T>
+{
+    int min  = 0;
+    int max = inputArray.Length - 1;
+
+    while (min <= max)
+    {
+        int mid = (min + max) / 2;
+        int comparison = key.CompareTo(inputArray[mid]);
+
+        if(comparison == 0)
+        {
+            return mid + 1;
+        } else if (comparison < 0)
+        {
+            max = mid - 1;
+        } else
+        {
+            min = mid - 1;
+        }
+    }
+
+    return "Nil";
+}
+
+static object BinarySearchRecursive<T>(T[] inputArray, T key, int min, int max) where T : IComparable<T>
+{
+    if (min > max)
+    {      
+        return "Nil";
+    } else
+    {
+        int mid = (min + max) / 2;
+        int comparison = key.CompareTo(inputArray[mid]);
+
+        if(comparison == 0)
+        {
+            return mid + 1;
+        } else if (comparison < 0)
+        {
+            return BinarySearchRecursive(inputArray, key, min, mid - 1);
+        } else
+        {
+            return BinarySearchRecursive(inputArray, key, mid + 1, max);
+        }
+    }
+}
+
+int[] numeros = {2, 4, 6, 8, 10};
+Console.WriteLine(BinarySearchIterative(numeros, 8));
+
+string[] nomes = {"Ana", "Bruno", "Carlos", "Diana"};
+Console.WriteLine(BinarySearchIterative(nomes, "Carlos"));
+
+double[] valores = {1.1, 2.2, 3.3, 4.4, 5.5};
+Console.WriteLine(BinarySearchIterative(valores, 7.7));
+
+Console.WriteLine(BinarySearchRecursive(numeros, 6, 0, numeros.Length - 1));
+Console.WriteLine(BinarySearchRecursive(nomes, "Carlos", 0, nomes.Length - 1));
+Console.WriteLine(BinarySearchRecursive(valores, 7.7, 0, valores.Length - 1));
---
+++ Busca Sentinela/Sentinela/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] numeros = {10, 20, 180, 30, 60, 50, 110, 100, 70};
+int keyInt = 180;
+SentinelSearch(numeros, numeros.Length, keyInt);
+
+string[] nomes = {"Ana", "Bruno", "Carlos", "Diana"};
+string keyString = "Carlos";
+SentinelSearch(nomes, nomes.Length, keyString);
+
+static void SentinelSearch<T>(T[] arr, int n, T key)
+{
+    T last = arr[n - 1];
+
+    arr[n - 1] = key;
+    int i = 0;
+
+    while(!EqualityComparer<T>.Default.Equals(arr[i], key))
+        i++;
+
+    arr[n - 1] = last;
+
+    if (i < n - 1 || EqualityComparer<T>.Default.Equals(arr[n - 1], key))
+        Console.WriteLine($"{key} está presente no índice {i}");
+    else
+        Console.WriteLine("Elemento não encontrado!");
+}
