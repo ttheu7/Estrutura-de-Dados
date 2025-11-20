cff44ef80ba60e8cc171c3acfba0eafdad998868
Matheus Gonçalves <102565900+ttheu7@users.noreply.github.com>
2025-11-19 21:45:06 -0300

    Dictionary and HashTable

---
+++ Dicionarios/Program.cs
@@ -0,0 +1,21 @@
+﻿
+
+Dictionary<string, int> dic = new Dictionary<string, int>
+{
+    {"Chave 1", 100},
+    {"Chave 2", 200}
+};
+
+dic["Chave 3"] = 150; 
+dic.Add("Chave 4", 300);
+
+if(dic.ContainsKey("Chave 4"))
+{
+    Console.WriteLine($"Encontrou a chave: Chave 4 com valor {dic["Chave 4"]}");
+}
+
+foreach(KeyValuePair<string, int> kvp in dic)
+{
+    Console.WriteLine($"Chave: {kvp.Key}, Valor: {kvp.Value}");
+}
+
---
+++ HashTable/HashTable/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+
+Hashtable ht = new Hashtable()
+{
+  {"Matheus","123"},  //chave-valor
+  {"Ttheu","456"}  
+};
+
+ht["Sutema"] = "789"; //adiciona
+ht.Add("Mago", "777"); //adiciona
+
+if(ht.Count > 0)
+{
+    foreach (DictionaryEntry entry in ht)
+    {
+        Console.WriteLine($"Chave {entry.Key}, Valor: {entry.Value}");
+    }
+} else
+{
+    Console.WriteLine("Tabelha Hash vazia!");
+}
+
+/*Adicionar chave repetida à tabela, o programa interrompe a execução! Por isso usamos try {} e catch {}*/
+
+Console.WriteLine("Informe a chave: ");
+string ?key = Console.ReadLine();
+
+Console.WriteLine("Informe o valor: ");
+string ?value = Console.ReadLine();
+
+try
+{
+    ht.Add(key!, value);
+    Console.WriteLine("Adicionado!");
+}
+catch
+{
+    Console.WriteLine("Não é possível adicionar chaves iguais!");
+}
+
+Console.Write("O que procuras: ");
+string ?search = Console.ReadLine();
+
+if (ht.Contains(search!))
+{
+    Console.WriteLine($"Encontrado {search}, {ht[search!  ]}");
+} else
+{
+    Console.WriteLine($"{search} não existe na tabela!");
+}
